e8e7f21f37cfcd6f5cf50924d22e4dd48aae9ced
vesvese55x <138940640+vesvese55x@users.noreply.github.com>
2025-02-15 21:05:47 +0300

    Update startwm.sh

--- startwm.sh
+++ startwm.sh
@@ -6,9 +6,8 @@ startxfce4 &
 # XFCE oturumu başladıktan sonra masaüstü arka planını değiştirmek için bekle
 (
 sleep 5
-setxkbmap tr
     # Arka plan resmini indir
-    wget -q -O /tmp/resim.jpg https://i.ibb.co/JFWSj6Cc/resim.jpg
+    wget -q -O /tmp/resim.jpg https://i.ibb.co/JFWSj6Cc/resim.jpg && setxkbmap tr
 
     # Bütün çalışma alanları için arka planı değiştir
 for workspace in $(xfconf-query -c xfce4-desktop -l | grep "workspace.*/last-image"); do xfconf-query -c xfce4-desktop -p "$workspace" -s "/tmp/resim.jpg"; done
