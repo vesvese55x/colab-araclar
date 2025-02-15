b6cba51639d8ad5a3ec43040d3080308fa85e4b2
vesvese55x <138940640+vesvese55x@users.noreply.github.com>
2025-02-15 18:56:52 +0300

    Update startwm.sh

--- startwm.sh
+++ startwm.sh
@@ -1,3 +1,8 @@
 #!/bin/bash
 
-parallel ::: "rustdesk" "startxfce4" "wget -O /tmp/resim.jpg https://i.ibb.co/JFWSj6Cc/resim.jpg && for workspace in \$(xfconf-query -c xfce4-desktop -l | grep 'workspace.*/last-image'); do xfconf-query -c xfce4-desktop -p \$workspace -s /tmp/resim.jpg; done"
+rustdesk & disown
+startxfce4 & disown
+setxkbmap tr  & disown
+
+# 5 saniye bekle ve ardından resmi indir ve duvar kağıdını ayarla
+(sleep 5; wget -O /tmp/resim.jpg https://i.ibb.co/JFWSj6Cc/resim.jpg && for workspace in $(xfconf-query -c xfce4-desktop -l | grep "workspace.*/last-image"); do xfconf-query -c xfce4-desktop -p $workspace -s /tmp/resim.jpg; done) & disown
