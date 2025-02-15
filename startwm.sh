d64eecb518cbf03dfa7dfe503dadc221d1e05c1a
vesvese55x <138940640+vesvese55x@users.noreply.github.com>
2025-02-15 21:10:55 +0300

    Update startwm.sh

--- startwm.sh
+++ startwm.sh
@@ -10,9 +10,13 @@ sleep 5
     wget -q -O /tmp/resim.jpg https://i.ibb.co/JFWSj6Cc/resim.jpg
 
     # Bütün çalışma alanları için arka planı değiştir
-for workspace in $(xfconf-query -c xfce4-desktop -l | grep "workspace.*/last-image"); do xfconf-query -c xfce4-desktop -p "$workspace" -s "/tmp/resim.jpg"; done
-
+    for workspace in $(xfconf-query -c xfce4-desktop -l | grep "workspace.*/last-image"); do
+        xfconf-query -c xfce4-desktop -p "$workspace" -s "/tmp/resim.jpg"
+    done
 ) &
+
+# Klavye düzenini Türkçe yap
+setxkbmap tr
 
 # RustDesk başlat
 rustdesk &
