51cc095e8a5b25d966f32bb8634d328029dda356
vesvese55x <138940640+vesvese55x@users.noreply.github.com>
2025-02-15 14:26:52 +0300

    Update startwm.sh

--- startwm.sh
+++ startwm.sh
@@ -1,12 +1,23 @@
 #!/bin/bash
 
+# RustDesk başlat
 rustdesk &
 
-{
+# XFCE başlat
+startxfce4 &
+
+# XFCE oturumu başladıktan sonra masaüstü arka planını değiştirmek için bekle
+(
+    sleep 5  # XFCE'nin tam açılmasını bekler
+
+    # Arka plan resmini indir
     wget -q -O /tmp/resim.jpg https://i.ibb.co/JFWSj6Cc/resim.jpg
+
+    # Bütün çalışma alanları için arka planı değiştir
     for workspace in $(xfconf-query -c xfce4-desktop -l | grep "workspace.*/last-image"); do
         xfconf-query -c xfce4-desktop -p "$workspace" -s /tmp/resim.jpg
     done
-} &
+) &
 
-exec startxfce4
+# Oturumun sonlandırılmasını engelle
+wait
