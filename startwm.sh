dc822f1e2a7b944db9490457a44dd0b30d348a33
vesvese55x <138940640+vesvese55x@users.noreply.github.com>
2025-02-15 04:22:32 +0300

    Update startwm.sh

--- startwm.sh
+++ startwm.sh
@@ -1,1 +1,1 @@
-parallel ::: "rustdesk" "startxfce4" "setxkbmap tr" "wget -O /tmp/resim.jpg https://i.ibb.co/JFWSj6Cc/resim.jpg && for workspace in \$(xfconf-query -c xfce4-desktop -l | grep 'workspace.*/last-image'); do xfconf-query -c xfce4-desktop -p \$workspace -s /tmp/resim.jpg; done"
+parallel ::: "rustdesk" "startxfce4" "wget -O /tmp/resim.jpg https://i.ibb.co/JFWSj6Cc/resim.jpg && for workspace in \$(xfconf-query -c xfce4-desktop -l | grep 'workspace.*/last-image'); do xfconf-query -c xfce4-desktop -p \$workspace -s /tmp/resim.jpg; done"
