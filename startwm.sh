bee974b2d55c51854b5200f31ae40635f84cf198
vesvese55x <138940640+vesvese55x@users.noreply.github.com>
2025-02-15 20:36:31 +0300

    Update startwm.sh

--- startwm.sh
+++ startwm.sh
@@ -1,8 +1,25 @@
 #!/bin/bash
+# XRDP için XFCE4 başlatma, klavye düzenini ayarlama ve masaüstü yapılandırma komutları
 
-rustdesk &
+# Ortam değişkenlerini yükle
+export LANG=C.UTF-8
+export DISPLAY=:0
+
+# XFCE4 ve RustDesk başlat
 startxfce4 &
+rustdesk &
 
-(sleep 5 && setxkbmap tr && wget -O /tmp/resim.jpg https://i.ibb.co/JFWSj6Cc/resim.jpg && xfconf-query -c xfce4-desktop -l | grep "last-image" | xargs -I{} xfconf-query -c xfce4-desktop -p "{}" -s "/tmp/resim.jpg") &
+# Klavye düzenini Türkçe yap
+setxkbmap tr
 
-wait
+# Resmi indir
+wget -O /tmp/resim.jpg https://i.ibb.co/JFWSj6Cc/resim.jpg
+
+# Birkaç saniye bekleyerek oturumun tam açılmasını sağla
+sleep 5
+
+# Masaüstü arka planını değiştir
+xfconf-query -c xfce4-desktop -l | grep "last-image" | xargs -I{} xfconf-query -c xfce4-desktop -p "{}" -s "/tmp/resim.jpg"
+
+# İşlemleri bitir ve oturumun çalışmasını sağla
+exec /bin/sh /etc/X11/Xsession
