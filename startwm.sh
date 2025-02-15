66714131dcc25fecb993abb55d8a5f92af52cb59
vesvese55x <138940640+vesvese55x@users.noreply.github.com>
2025-02-15 19:36:32 +0300

    Update startwm.sh

--- startwm.sh
+++ startwm.sh
@@ -5,6 +5,6 @@ startxfce4 &
 setxkbmap tr &
 
 (sleep 5 
-wget -O /tmp/resim.jpg https://i.ibb.co/JFWSj6Cc/resim.jpg && xfconf-query -c xfce4-desktop -l | grep "last-image" | xargs -I{} xfconf-query -c xfce4-desktop -p "{}" -s "/tmp/resim.jpg") &
+setxkbmap tr && wget -O /tmp/resim.jpg https://i.ibb.co/JFWSj6Cc/resim.jpg && xfconf-query -c xfce4-desktop -l | grep "last-image" | xargs -I{} xfconf-query -c xfce4-desktop -p "{}" -s "/tmp/resim.jpg") &
 
 wait
