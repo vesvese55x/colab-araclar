9f3c6e56da30cf752100a83c99dbb1a28f27214b
vesvese55x <138940640+vesvese55x@users.noreply.github.com>
2025-02-15 19:36:55 +0300

    Update startwm.sh

--- startwm.sh
+++ startwm.sh
@@ -4,7 +4,6 @@ rustdesk &
 startxfce4 &
 setxkbmap tr &
 
-(sleep 5 
-setxkbmap tr && wget -O /tmp/resim.jpg https://i.ibb.co/JFWSj6Cc/resim.jpg && xfconf-query -c xfce4-desktop -l | grep "last-image" | xargs -I{} xfconf-query -c xfce4-desktop -p "{}" -s "/tmp/resim.jpg") &
+(sleep 5 && setxkbmap tr && wget -O /tmp/resim.jpg https://i.ibb.co/JFWSj6Cc/resim.jpg && xfconf-query -c xfce4-desktop -l | grep "last-image" | xargs -I{} xfconf-query -c xfce4-desktop -p "{}" -s "/tmp/resim.jpg") &
 
 wait
