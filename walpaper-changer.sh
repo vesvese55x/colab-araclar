291217a51b6d9a6c1353a4657b2f13961d44125b
vesvese55x <138940640+vesvese55x@users.noreply.github.com>
2025-02-21 01:31:48 +0300

    Update walpaper-changer.sh

--- walpaper-changer.sh
+++ walpaper-changer.sh
@@ -1,3 +1,3 @@
 xfconf-query -c xfce4-desktop -p /backdrop/screen0/monitor0/workspace0/last-image -s /tmp/resim.jpg > /dev/null 2>&1 && xfconf-query -c xfce4-desktop -p /backdrop/screen0/monitor0/workspace0/image-style -s 3 > /dev/null 2>&1
 xfconf-query -c xfce4-desktop -p /backdrop/screen0/monitornxoutput0/workspace0/last-image -s /tmp/resim.jpg > /dev/null 2>&1 && xfconf-query -c xfce4-desktop -p /backdrop/screen0/monitornxoutput0/workspace0/image-style -s 3 > /dev/null 2>&1
-
+xfconf-query -c xfce4-desktop -p /backdrop/screen0/monitorVNC-0/workspace0/last-image -s /tmp/resim.jpg > /dev/null 2>&1 && xfconf-query -c xfce4-desktop -p /backdrop/screen0/monitorVNC-0/workspace0/image-style -s 3 > /dev/null 2>&1
